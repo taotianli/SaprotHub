9a0f1c57f9c8572e478d6fa946217c498c9e80d1
tenley <1105747520@qq.com>
2025-05-02 21:48:36 +0800

    modefy the dataset

--- saprot/dataset/saprot/saprot_classification_dataset.py
+++ saprot/dataset/saprot/saprot_classification_dataset.py
@@ -41,6 +41,7 @@ class SaprotClassificationDataset(LMDBDataset):
     def __getitem__(self, index):
         entry = json.loads(self._get(index))
         seq = entry['seq']
+        print("seq", seq)
         if not isinstance(self.tokenizer, EsmTokenizer):
             seq = " ".join(seq)
         
